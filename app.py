036160ffa7cbe9cb85acdb265d560948b3d80423
Mafika <rudolphmafika@gmail.com>
2025-07-06 11:33:00 +0200

    Test DB connection2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ port = int(os.environ.get("PORT", 5000))
 
 @app.route("/")
 def main():
-    status = test_connection()
+    #status = test_connection()
     return f"Status: "
 
 if __name__ == "__main__":
