2634fef0a2de1d13ac16e419628c052c2bf60548
Mafika <rudolphmafika@gmail.com>
2025-07-06 14:26:11 +0200

    Test DB connection

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
 import os
-from services.compute_results import total_surveys
+from services.DBconnection import get_DBconnection
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 
 @app.route("/")
 def main():
-    return f"Total Surveys: {total_surveys()}"
+    conn = get_DBconnection()
+    return f"Total Surveys: {conn}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
